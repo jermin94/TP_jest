394e901d2e8256b0fd328324fc8b2aea8f9e36a7
Jermin94 <jer5@live.fr>
2019-04-02 12:17:17 +0200

    Add all files

---
+++ stryker.conf.js
@@ -0,0 +1,15 @@
+module.exports = function(config) {
+  config.set({
+    mutator: "javascript",
+    packageManager: "npm",
+    reporters: ["clear-text", "progress"],
+    testRunner: "jest",
+    transpilers: [],
+    coverageAnalysis: "off",
+    thresholds: {
+     break: 80, // Fail if mutation score < 70
+     // ..
+   }
+  });
+};
+
